63ca1a1824efef807b24846ff7f1fa0eedf43ab3
Your Name <you@example.com>
2025-10-15 21:26:06 +0700

    First Commit

---
+++ Lesson 7/Area/Area.js
@@ -0,0 +1,4 @@
+let width = prompt('Hãy nhập chiều dài hình chữ nhật');
+let height = prompt('Hãy nhập chiều rộng hình chữ nhật')
+let area = parseInt(width) * parseInt(height);;
+document.write('Diện tích hình chữ nhật là: '+ area)
