783dfa24dcb84c8b89dfbb55ad8b4d096528e556
Your Name <you@example.com>
2025-10-16 15:59:12 +0700

    First Commit

---
+++ Lesson 7/BT2/Convert.js
@@ -0,0 +1,20 @@
+function Convert() {
+let a = parseInt(document.getElementById("amount").value);
+let c1 = document.getElementById("cur1").value;
+let c2 = document.getElementById("cur2").value;
+var r;
+if (c1 == "USD") {
+    if (c2 == "USD") {
+        r = a;
+    } else if (c2 == "VND") {
+        r = a * 27000;
+    }
+}
+else if (c1 == "VND") {
+    if (c2 == "VND") {
+        r = a;
+    } else if (c2 == "USD") {
+        r = a / 27000;
+    }
+}
+document.getElementById("result").innerHTML = 'Result: ' + r + ' ' + c2;}
