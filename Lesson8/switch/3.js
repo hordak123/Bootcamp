bc2bd07832658ae0de298a4163139503efde2560
Your Name <you@example.com>
2025-10-17 20:53:20 +0700

    First Commit

---
+++ Lesson8/switch/3.js
@@ -0,0 +1,20 @@
+let value = +prompt('Nhập vào một số', 0);
+if (value > 0) {
+    alert(1);
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+// switch (value) {
+//     case 0:
+//         alert(0);
+//         break;
+//     case (value > 0):
+//         alert(1);
+//         break;
+//     case (value < 0):
+//         alert(-1);
+//         break;
+// }
