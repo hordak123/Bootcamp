6ca0562862bf0bbf383f90f3857f99cbd1da9c36
Your Name <you@example.com>
2025-11-03 16:34:45 +0700

    Reorganize folder structure

---
+++ Bootcamp/README.md
@@ -0,0 +1,6 @@
+>> Giải nén file vừa tải về >> Chạy file "Set-up.exe" để cài đặt
+>> Install >> Continue
+>> Đợi phần mềm cài đặt (100%) >> Close
+>> Mở phần mềm lên sử dụng
+
+           Chúc các bạn thành công!
---
+++ Lesson 7/script.js
@@ -0,0 +1,15 @@
+// vả, let, const
+
+var HoTen = 'Trần Quang Huy';
+var Tuoi = 28;
+var KetHon = true;
+console.log(HoTen);
+console.log(Tuoi);
+console.log(KetHon);
+
+let HovaTen = 'Tran Quang Huy';
+HovaTen = 'Tran Quang Khoi';
+console.log(HovaTen);
+
+const PI = Math.PI;
+console.log(PI);
